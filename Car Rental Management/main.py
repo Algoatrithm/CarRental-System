62f8e632dcb39d6f52d08adc0aeb193035919590
Allen <penanoallen@gmail.com>
2025-05-06 17:42:13 +0800

    Co-authored-by: Rjannn <Rjannn@users.noreply.github.com>

--- Car Rental Management/main.py
+++ Car Rental Management/main.py
@@ -3,6 +3,26 @@ from tkinter import ttk
 class Main:
     def __init__(self):
         pass
+    # ===============================
+    available_vehicles: list = [
+        {
+            "vehicle" : "Bicycle", 
+            "id" : 0, 
+            "brand" : "Celt2k24",
+            "model" : "Fixie", 
+            "status" : "Available",
+            "visible" : True,
+        },
+        {
+            "vehicle" : "Motorcycle", 
+            "id" : 11, 
+            "brand" : "Honda",
+            "model" : "XRM", 
+            "status" : "Available",
+            "visible" : True,
+        },
+    ]
+        # ===============================
     def Program(self):
         # Root Window
         root = tk.Tk()
@@ -12,18 +32,6 @@ class Main:
         # Configure grid for main window (rows/columns)
         root.columnconfigure(0, weight=1)
         root.rowconfigure(1, weight=1)
-
-        # ===============================
-        available_vehicles: list = [
-            {
-                "vehicle" : "Bicycle", 
-                "id" : 0, 
-                "brand" : "Celt2k24",
-                "model" : "Fixie", 
-                "status" : "Available",
-            },
-        ]
-        # ===============================
 
         # ===============================
         # Top Navigation Bar
@@ -41,7 +49,6 @@ class Main:
         main_frame.columnconfigure(0, weight=1)
         main_frame.columnconfigure(1, weight=2)
         main_frame.rowconfigure(0, weight=1)
-aSVEGb g
         # ===============================
         # Left Panel – Vehicle Selection
         # ===============================
@@ -49,7 +56,7 @@ aSVEGb g
         vehicle_selection_frame.grid(row=0, column=0, sticky="nswe", padx=(0, 10))
 
         ttk.Label(vehicle_selection_frame, text="Vehicle Type:").pack(anchor="w", pady=(0, 5))
-        vehicle_type_combo = ttk.Combobox(vehicle_selection_frame, values=available_vehicles)
+        vehicle_type_combo = ttk.Combobox(vehicle_selection_frame, values=self.available_vehicles[0]["vehicle"])
         vehicle_type_combo.pack(fill="x", pady=(0, 10))
 
         search = ttk.Button(vehicle_selection_frame, text="Search", command=lambda: self.show_selected_vehicle(vehicle_type_combo.get()) ).pack(fill="x")
@@ -71,10 +78,11 @@ aSVEGb g
         vehicle_tree.pack(fill="both", expand=True)
 
         # Display vehicles
-        for vehicle in available_vehicles:
-            column = (vehicle["vehicle"], vehicle["id"], vehicle["brand"], vehicle["model"], vehicle["status"])
-            vehicle_tree.insert('', 'end', values=column)
-        
+        for vehicle in self.available_vehicles:
+            if vehicle["visible"] == True:
+                column = (vehicle["vehicle"], vehicle["id"], vehicle["brand"], vehicle["model"], vehicle["status"])
+                vehicle_tree.insert('', 'end', values=column)
+                print(vehicle)
 
         # ===============================
         # Bottom Status Bar (optional)
@@ -84,9 +92,13 @@ aSVEGb g
 
         # Start mainloop
         root.mainloop()
+
     def show_selected_vehicle(self, searched_vehicle):
-        if (searched_vehicle == "Bicycle"):
-            print("LKEDIHE")
+        for vehicle in self.available_vehicles:
+            if vehicle["vehicle"] != searched_vehicle:
+                vehicle["visible"] = False
+                print(vehicle["visible"])
+        
         
 
 main = Main()
